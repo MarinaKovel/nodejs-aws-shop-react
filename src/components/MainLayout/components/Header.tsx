48b0b8f085553f1f435abe3da39169ee2133a290
Marina Kovel <m.kovel@softteco.com>
2025-02-26 11:28:40 +0300

    refactor: remove errors, change styles and image source

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -25,7 +25,7 @@ export default function Header() {
 
   return (
     <AppBar position="relative">
-      <Toolbar>
+      <Toolbar sx={{ backgroundColor: '#4E5754' }}>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           <Link
             component={RouterLink}
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My AWS Store!
+            AWS Painting Store
           </Link>
         </Typography>
 
