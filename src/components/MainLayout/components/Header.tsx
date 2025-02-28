b7a45b7fc6a4f3b72d9886d4ec1af4cb824c3773
Marina Kovel <m.kovel@softteco.com>
2025-02-28 11:41:40 +0300

    feat: change theme

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -25,7 +25,7 @@ export default function Header() {
 
   return (
     <AppBar position="relative">
-      <Toolbar sx={{ backgroundColor: '#4E5754' }}>
+      <Toolbar>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           <Link
             component={RouterLink}
