fe43171427ec28c408cd548f09853317b33720b3
Marina Kovel <m.kovel@softteco.com>
2025-04-14 14:56:21 +0300

    feat: update url to handle https

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://zd34wens4b.execute-api.eu-central-1.amazonaws.com/prod",
-  order: "http://marinakovel-cart-api-prod.eu-central-1.elasticbeanstalk.com/api/profile/cart",
+  order: "https://d13x8n7goajpcj.cloudfront.net/api/profile/cart",
   import: "https://7so3qk9a67.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "http://marinakovel-cart-api-prod.eu-central-1.elasticbeanstalk.com/api",
+  cart: "https://d13x8n7goajpcj.cloudfront.net/api",
 };
 
 export default API_PATHS;
